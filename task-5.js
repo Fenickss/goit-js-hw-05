c951c6cea6e8dd9a18fd8266d709d08acbd09cac
Fenickss <denis.suxov.1991@mail.ru>
2022-08-04 08:30:35 +0300

    конец

--- task-5.js
+++ task-5.js
@@ -23,8 +23,8 @@
 //   constructor() {}
 
 //   /*
-//    * Добавь геттер и сеттер для свойства price,
-//    * который будет работать с свойством цены автомобиля.
+//    * Добавь геттер и сеттер для свойства price,?????????????????????????????????????
+//    * который будет работать с свойством цены автомобиля.???????????????????????????????
 //    */
 
 //   /*
@@ -80,3 +80,82 @@
 // mustang.price = 4000;
 // console.log(mustang.price); // 4000
 // ```
+
+//================================================================
+//=====================================================================
+
+class Car {
+  static getSpecs(car) {
+    console.log(
+      `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`
+    );
+  }
+
+  constructor({ speed, price, maxSpeed, isOn, distance }) {
+    this.speed = speed,
+      this.price = price,
+      this.maxSpeed = maxSpeed,
+      this.isOn = isOn,
+      this.distance = distance;
+  }
+
+  // get price() {
+  //   return this.#price;
+  // }
+
+  // set price(newPrice) {
+  //   this.#price = newPrice;
+  // }
+
+  turnOn() {
+    this.isOn = true;
+    this.speed = 0;
+  }
+
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+
+  accelerate(value) {
+    if (this.speed + value <= this.maxSpeed) {
+      this.speed += value;
+    }
+  }
+
+  decelerate(value) {
+    if (this.speed - value > 0) {
+      this.speed -= value;
+    }
+  }
+
+  drive(hours) {
+    if (this.isOn) {
+      this.distance = hours * this.speed
+    }
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+////////// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+////////// maxSpeed: 200, speed: 0, isOn: false, distance: 30, price: 2000
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
+
+
+
+
